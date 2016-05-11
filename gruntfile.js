d059b701f8f16a6824069f60e2d003adfbe7d25e
Alla Logozinskaya <logozynski@gmail.com>
2016-05-11 20:00:44 +0300

    new

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+﻿module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+  options: {
+    // define a string to put between each file in the concatenated output
+    separator: ';'
+  },
+  dist: {
+    // the files to concatenate
+    src: ['js/*.js'],
+    // the location of the resulting JS file
+    dest: 'js/script.main.js'
+  }
+},
+uglify: {
+	dist:{
+		src: ['js/script.main.js'],
+		dest: 'js/script.main.min.js'
+	}
+},
+cssmin: {
+  target: {
+    files: [{
+      expand: true,
+      //cwd: 'release/css',
+      src: ['css/*.css'],
+      dest: 'css/style.min.css',
+      //ext: '.min.css'
+    }]
+  }
+}
+
+  });
+
+ 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+};
